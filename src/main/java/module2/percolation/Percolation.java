f45d83fa2effa88c7ced22a94c4ab085b9e97d7a
yoshiohasegawa <yoshio.seisuke.hasegawa@gmail.com>
2026-01-06 15:19:42 +0100

    Simplify Percolation
    Remove the public print method.
    
    Rework the main method.

--- src/main/java/module2/percolation/Percolation.java
+++ src/main/java/module2/percolation/Percolation.java
@@ -147,67 +147,13 @@ public class Percolation {
         return this.percolationQU.find(0) == this.percolationQU.find(((this.size * this.size) + 1));
     }
 
-    // Print the grid to standard output
-    public void print() {
-        System.out.println("Grid of size " + this.size);
-        System.out.println();
-
-        System.out.println("Index:");
-        for (int i = 0; i < this.size; i++) {
-            for (int j = 0; j < this.size; j++) {
-                System.out.print(this.idxGrid[i][j]);
-                if (this.idxGrid[i][j] < 10) {
-                    System.out.print("   ");
-                } else if (this.idxGrid[i][j] < 100) {
-                        System.out.print("  ");
-                } else if (this.idxGrid[i][j] < 1000) {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        System.out.println("Vacancy:");
-        for (int i = 0; i < this.size; i++) {
-            for (int j = 0; j < this.size; j++) {
-                if (this.vacancyGrid[i][j]) {
-                    System.out.print("o ");
-                } else {
-                    System.out.print("x ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        System.out.println("Represented as Quick Union:");
-        int count = 1;
-        for (int i = 1; i <= this.size; i++){
-            for (int j = 1; j <= this.size; j++) {
-                int parent = this.percolationQU.find(count);
-                System.out.print(this.percolationQU.find(count));
-                if (parent < 10) {
-                    System.out.print("   ");
-                } else if (parent < 100) {
-                        System.out.print("  ");
-                } else if (parent < 1000) {
-                    System.out.print(" ");
-                }
-                count++;
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
     // test client (optional)
     public static void main(String[] args) {
         int n = 3;
         Percolation p = new Percolation(n);
 
+        System.out.println("Grid of size " + n);
         System.out.println();
-        p.print();
 
         System.out.println("Number of open sites: " + p.numberOfOpenSites());
         System.out.println("Percolates: " + p.percolates());
@@ -220,7 +166,6 @@ public class Percolation {
         p.open(3, 1);
 
         System.out.println();
-        p.print();
 
         System.out.println("Number of open sites: " + p.numberOfOpenSites());
         System.out.println("Percolates: " + p.percolates());
@@ -233,7 +178,6 @@ public class Percolation {
         p.open(2, 1);
 
         System.out.println();
-        p.print();
 
         System.out.println("Number of open sites: " + p.numberOfOpenSites());
         System.out.println("Percolates: " + p.percolates());
@@ -246,7 +190,6 @@ public class Percolation {
         p.open(1, 3);
 
         System.out.println();
-        p.print();
 
         System.out.println("Number of open sites: " + p.numberOfOpenSites());
         System.out.println("Percolates: " + p.percolates());
@@ -259,7 +202,6 @@ public class Percolation {
         p.open(2, 3);
 
         System.out.println();
-        p.print();
 
         System.out.println("Number of open sites: " + p.numberOfOpenSites());
         System.out.println("Percolates: " + p.percolates());
@@ -272,156 +214,10 @@ public class Percolation {
         p.open(2, 2);
 
         System.out.println();
-        p.print();
 
         System.out.println("Number of open sites: " + p.numberOfOpenSites());
         System.out.println("Percolates: " + p.percolates());
         System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("New Percolation instance");
-        Percolation newP = new Percolation(n);
-
-        System.out.println();
-        newP.print();
-
-        System.out.print("Site [2, 2] status: ");
-        if (newP.isFull(2, 2)) {
-            System.out.println("Full");
-        } else {
-            System.out.println("Empty");
-        }
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [2, 2]...");
-        newP.open(2, 2);
-
-        System.out.println();
-        newP.print();
-        
-        System.out.print("Site [2, 2] status: ");
-        if (newP.isFull(2, 2)) {
-            System.out.println("Full");
-        } else {
-            System.out.println("Empty");
-        }
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [2, 3]...");
-        newP.open(2, 3);
-
-        System.out.println();
-        newP.print();
-        
-        System.out.print("Site [2, 2] status: ");
-        if (newP.isFull(2, 2)) {
-            System.out.println("Full");
-        } else {
-            System.out.println("Empty");
-        }
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [1, 3]...");
-        newP.open(1, 3);
-
-        System.out.println();
-        newP.print();
-        
-        System.out.print("Site [2, 2] status: ");
-        if (newP.isFull(2, 2)) {
-            System.out.println("Full");
-        } else {
-            System.out.println("Empty");
-        }
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("New, bigger Percolation instance");
-        int m = 4;
-        Percolation biggerP = new Percolation(m);
-
-        System.out.println();
-        biggerP.print();
-
-        System.out.println("Number of open sites: " + biggerP.numberOfOpenSites());
-        System.out.println("Percolates: " + biggerP.percolates());
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [1, 1]...");
-        biggerP.open(1, 1);
-
-        System.out.println();
-        biggerP.print();
-
-        System.out.println("Number of open sites: " + biggerP.numberOfOpenSites());
-        System.out.println("Percolates: " + biggerP.percolates());
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [2, 1]...");
-        biggerP.open(2, 1);
-
-        System.out.println();
-        biggerP.print();
-
-        System.out.println("Number of open sites: " + biggerP.numberOfOpenSites());
-        System.out.println("Percolates: " + biggerP.percolates());
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [3, 1]...");
-        biggerP.open(3, 1);
-
-        System.out.println();
-        biggerP.print();
-
-        System.out.println("Number of open sites: " + biggerP.numberOfOpenSites());
-        System.out.println("Percolates: " + biggerP.percolates());
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [4, 1]...");
-        biggerP.open(4, 1);
-
-        System.out.println();
-        biggerP.print();
-
-        System.out.println("Number of open sites: " + biggerP.numberOfOpenSites());
-        System.out.println("Percolates: " + biggerP.percolates());
-        System.out.println();
-
-        System.out.println("---------------------------");
-        System.out.println();
-
-        System.out.println("Opening site [4, 3]...");
-        biggerP.open(4, 3);
-
-        System.out.println();
-        biggerP.print();
-
-        System.out.println("Is [4, 3] full?: " + biggerP.isFull(4, 3));
     }
 }
     
